3aa47b691b10c8f0a3451c3d1d6c9f7b53a10610
Francis9817 <61259614+Francis9817@users.noreply.github.com>
2021-01-18 09:07:04 -0500

    Add files via upload

---
+++ HilosPool/HilosPool/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace HilosPool
+{
+    class Program
+    {
+        const string path = @"C:\Users\Francisco\Desktop\Hilos\";
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Create, i);
+            }
+            while (ThreadPool.PendingWorkItemCount > 0) ;
+
+        }
+        static void Create(object data)
+        {
+            int i = (int)data;
+            using (var sw = new StreamWriter(path + i + "txt"))
+            {
+                sw.WriteLine("Hola soy el hilo " + Thread.CurrentThread.ManagedThreadId);
+            }
+            Console.WriteLine("Hola soy el hilo " + Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
